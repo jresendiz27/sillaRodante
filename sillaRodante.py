b428c297f362cc1b915bddfd8760971b1009c394
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2014-07-15 20:53:57 -0500

    Prueba

--- sillaRodante.py
+++ sillaRodante.py
@@ -1,10 +1,15 @@
-import cgi
-import urllib
-#para generar los archivos json!!!
+# para generar los archivos json!!!
 import json
-#para generar un template de String
+# para generar un template de String
 from string import Template
-from google.appengine.ext import ndb
+# Utileria
+from util.Util import Util
+# Modelos de la base de datos
+from modelos.PuntoClave import PuntoClave
+#Herramienta para el manejo de mapas
+from util.MapTool import MapTool
+from util.Punto import Punto
+import logging as logger
 
 import webapp2
 
@@ -57,97 +62,10 @@ OBTENER_PUNTOS_ENTRE_AREA_FORM = """
     </form>
     <iframe id="respuesta" name="respuesta" width="100%" height="100%"></iframe>
 """
-class Util:
-    #Metodo Web para obtener los puntos entre cierto rango
-    def obtenerAreaDeBusqueda(self,punto1,punto2):
-        #lista de los puntos cardinales
-        puntosCardinales = ['norte','sur','este','oeste']
-        puntosMasLejanos = {}
-        puntosMasLejanos['latitudMaxima'] = 0.0
-        puntosMasLejanos['longitudMaxima'] = 0.0
-        puntosMasLejanos['latitudMinima'] = 0.0
-        puntosMasLejanos['longitudMinima'] = 0.0
-        #Se considera una diferencia de 0.0001 para los rangos, Gracias a Ricardo por ayudar a determinar la ubicacion del punto
-        diferencia = 0.0001
-        #Se determina la ubicacion por una comparacio, gracias a Joaquin por ayudar a la visualizacion de los puntos en un plano x,y
-        if punto1.latitud >= punto2.latitud:
-            #Punto 1 arriba de punto 2
-            #Se calculan el maximo y el minimo de las latitudes
-            puntosMasLejanos['latitudMaxima'] = punto1.latitud + diferencia
-            puntosMasLejanos['latitudMinima'] = punto2.latitud - diferencia
-        else:
-            #Punto 2 arriba de punto 1
-            #Se calculan el maximo y el minimo de las latitudes
-            puntosMasLejanos['latitudMaxima'] = punto2.latitud + diferencia
-            puntosMasLejanos['latitudMinima'] = punto1.latitud - diferencia
-        #
-        #Se determina la ubicacion de la longitud
-        #
-        if punto1.longitud >= punto2.longitud:
-            #Punto 1 a la derecha de punto 2
-            #Se calculan el maximo y el minimo de las longitudes
-            puntosMasLejanos['longitudMaxima'] = punto1.longitud + diferencia
-            puntosMasLejanos['longitudMinima'] = punto2.longitud - diferencia
-        else:
-            #Punto 1 a la derecha de punto 2
-            #Se calculan el maximo y el minimo de las longitudes
-            puntosMasLejanos['longitudMaxima'] = punto2.longitud + diferencia
-            puntosMasLejanos['longitudMinima'] = punto1.longitud - diferencia
-        return puntosMasLejanos
 
-    def obtenerPuntos(self,request):
-        punto1 = Punto()
-        punto2 = Punto()
-        #Se obtiene la ubicacion del cliente
-        latitudOrigen = float(request.get('latitudOrigen'))
-        longitudOrigen = float(request.get('longitudOrigen'))
-        #se asigna la informacion al punto
-        punto1.latitud = latitudOrigen
-        punto1.longitud = longitudOrigen
-        #Se obtiene el punto al que el cliente desea llegar
-        latitudDestino = float(request.get('latitudDestino'))
-        longitudDestino = float(request.get('longitudDestino'))
-        #se asigna la informacion al punto
-        punto2.latitud = latitudDestino
-        punto2.longitud = longitudDestino
-        #Se calculan las latitudes maximas y minimas llamando al metodo generado previamente
-        rangos = self.obtenerAreaDeBusqueda(punto1,punto2)
-        print "*"*60
-        print "\n"
-        print rangos
-        print "*"*60
-        print "\n"
-        #Se ejecuta la consulta con los parametros obtenidos, primero se filtra la latitud
-        puntosFiltro1 = PuntoClave.gql("""
-                    where 
-                        latitud <=:latMax 
-                    and 
-                        latitud >=:latMin 
-                        """,
-                        latMax = rangos['latitudMaxima'],
-                        latMin = rangos['latitudMinima']).fetch(100)
-        puntosFiltrados = []
-        #Se aplica un segundo filtro, limitantes de Datastore (NoSQL) por razones de indices.
-        #Se hace un fetch de ciertos puntos ya que se considera que no es necesario obtener todo.
-        for punto in puntosFiltro1:
-            if punto.longitud <= rangos['longitudMaxima'] and punto.longitud >= rangos['longitudMinima']:
-                puntosFiltrados.append(punto)
-
-        return puntosFiltrados
-class Punto:
-    def __init__(self):
-        self.latitud = 0.0
-        self.longitud = 0.0
-
-class PuntoClave(ndb.Model):
-    """Modelo de la tabla en la base de datos"""
-    latitud = ndb.FloatProperty()
-    longitud = ndb.FloatProperty()
-    valoracion = ndb.FloatProperty()
 
 class GuardarPuntosClavePage(webapp2.RequestHandler):
-    def get(self):  
-        print "Guardar puntos clave"
+    def get(self):
         mensajeExito = 'Guardaro Correctamente!'
         try:
             puntoAGuardar = PuntoClave()
@@ -155,27 +73,76 @@ class GuardarPuntosClavePage(webapp2.RequestHandler):
             puntoAGuardar.longitud = float(self.request.get('longitud'))
             puntoAGuardar.valoracion = int(self.request.get('valoracion'))
             puntoAGuardar.put()
-            print "Guardado!!!!"
-            self.response.write(MAIN_PAGE.substitute(mensaje=mensajeExito,formularioACargar=GUARDAR_PUNTOS_CLAVE_FORM,formularioConsulta=OBTENER_PUNTOS_ENTRE_AREA_FORM))
+            if bool(self.request.get('dev')):  #Si existe el parametro debug!, se genera la pagina web
+                print("Guardado!!!!")
+                self.response.write(
+                    MAIN_PAGE.substitute(mensaje=mensajeExito, formularioACargar=GUARDAR_PUNTOS_CLAVE_FORM,
+                                         formularioConsulta=OBTENER_PUNTOS_ENTRE_AREA_FORM))
+            #significa que solo necesitan la respuesta en JSON, posiblemente en produccion
+            else:
+                mapaRespuesta = {'mensaje': 'Guardado Correctamente!'}
+                self.response.headers['Content-Type'] = 'application/json'
+                self.response.out.write(json.dumps(mapaRespuesta))
+
             #self.redirect('/test')
         except Exception as e:
-            self.response.write(MAIN_PAGE.substitute(mensaje=e,formularioACargar=GUARDAR_PUNTOS_CLAVE_FORM,formularioConsulta=OBTENER_PUNTOS_ENTRE_AREA_FORM))
+            self.response.write(MAIN_PAGE.substitute(mensaje=e, formularioACargar=GUARDAR_PUNTOS_CLAVE_FORM,
+                                                     formularioConsulta=OBTENER_PUNTOS_ENTRE_AREA_FORM))
+
     def post(self):
         self.get()
 
+
 class MostrarPuntosClavePage(webapp2.RequestHandler):
     def get(self):
-        print "OK--------------------------------------'\n"
         utileria = Util()
         listadoDePuntos = utileria.obtenerPuntos(self.request)
         jsonAMostrar = json.dumps([punto.to_dict() for punto in listadoDePuntos])
         self.response.write(jsonAMostrar)
-        #self.redirect('/?' + urllib.urlencode(query_params))
+
+    #self.redirect('/?' + urllib.urlencode(query_params))
     def post(self):
         self.get()
 
+
+class ObtenerRutasPage(webapp2.RequestHandler):
+    def get(self):
+        #clases de utileria
+        utileria = Util()
+        #clase para las herramientas de mapas
+        mapTool = MapTool()
+        #puntoOrigen,puntoDestino = mapTool.obtenerPuntos(self.request)
+        puntoOrigen = None
+        puntoDestino = None
+        try:
+            puntoOrigen = Punto()
+            puntoDestino = Punto()
+            #Se asignan los valores de request
+            #Origen
+            puntoOrigen.latitud = float(self.request.get('latitudOrigen'))
+            puntoOrigen.longitud = float(self.request.get('longitudOrigen'))
+            #Destino
+            puntoDestino.latitud = float(self.request.get('latitudDestino'))
+            puntoDestino.longitud = float(self.request.get('longitudDestino'))
+        except Exception as e:
+            logger.error("No se pudieron convertir los parametros de la peticion a puntos geograficos.")
+            logger.error(e)
+        #Se imprime el error en caso de existir, y se regresan los objetos
+        puntosClave = utileria.obtenerPuntos(self.request)
+        logger.warn(puntoOrigen)
+        logger.warn(puntoDestino)
+        rutasPosibles = mapTool.obtenerRutasOptimasEntrePuntos(puntoOrigen,puntoDestino,puntosClave)
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(rutasPosibles)
+
+    #Un little hack para que funcione por ambos metodos,se usara POST, por motivos de encapsulamiento de la informacion
+    def post(self):
+        self.get()
+
+
 application = webapp2.WSGIApplication([
-    ('/test', GuardarPuntosClavePage),
-    ('/', GuardarPuntosClavePage),
-    ('/obtenerPuntos', MostrarPuntosClavePage)
-], debug=True)
+                                          ('/test', GuardarPuntosClavePage),
+                                          ('/', GuardarPuntosClavePage),
+                                          ('/obtenerPuntos', MostrarPuntosClavePage),
+                                          ('/obtenerRutas',ObtenerRutasPage )
+                                      ], debug=True)
